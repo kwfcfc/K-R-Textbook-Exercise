43452f68cac779e12e01a5135411809330a3cd6d
kwfcfc <73300175+kwfcfc@users.noreply.github.com>
2023-07-19 23:48:58 +0800

    feat: finish 1.23 delete comments
    - add deletetion for // inline_comments
    - change the check status for quotation
    - add macros for match function

--- Chapter 1/1.23 delete comments.c
+++ Chapter 1/1.23 delete comments.c
@@ -4,67 +4,64 @@
 #define OUTPAIR 0
 #define BUFFER_SIZE 3 /* length of BUFFER, plus the '\0' char, so the actual
                          length is 2. */
+#define MATCHED 1 /* used for match function */ 
+#define UNMATCHED 0
 
 int str_buffer(char string[], int length, int character);
+int match(char string[], char word[]);
 
 int main(){
   int c, i;
   char multi_comments[2][3]={"/*", "*/"};
-  // char inline_comments[2][3]={"//", "\n"};
-  int comment = OUTPAIR;
+  char inline_comments[2][3]={"//", "\n"};
+  int single_line = OUTPAIR;
+  int multi_line = OUTPAIR;
+  int comment = (single_line || multi_line);
   int quote = OUTPAIR;
   char buffer[BUFFER_SIZE];
-  /* char status_buffer[BUFFER_SIZE]; set a buffer to output the result, but it
-                                      is set to be char because of the
-                                      requiremnts of function. */
+
   for (i=0; i<BUFFER_SIZE; ++i)
     buffer[i] = '\0';
 
-
   while ((c = str_buffer(buffer, BUFFER_SIZE, getchar())) != EOF){
-    if ((comment == OUTPAIR) && (c == '\"' || c == '\'')) /* means it matches a quote, we don't check
-                                   whether " or ' matches each other. */
-      quote = (quote == OUTPAIR? INPAIR:OUTPAIR); /* switch the quote status */
-    /*
-    if (comment == INPAIR && match(buffer, inline_comments[1]) == 1)
-      comment = OUTPAIR;
-    */
+    if ((comment == OUTPAIR) && (c == '\"'))
+        /* means it matches a quote, there is no need to check whether " or '
+           matches each other. As in C Program, ' is only used to represent a
+           char constant instead of a string variable. */
+      quote = (quote == OUTPAIR? INPAIR:OUTPAIR); // switch the quote status
     if (quote == INPAIR){
+      /* this expression needs to check the quote status because in its
+         quotation it must read and write the same char. */
       putchar(c);
       continue;
     }else{
       if (comment == INPAIR){
-        for (i=0; i < BUFFER_SIZE; ++i)
-          if (buffer[i] != multi_comments[1][i])
-            break;
-        /* this uses a for loop to test whether it needs to jump out the
-           comments. the '\0' of string is not used due to the initialization of
-           the buffer to be all '\0'.
-        */
-        comment = (i == BUFFER_SIZE? OUTPAIR:INPAIR);
-        if (comment == OUTPAIR)
-          for (i=0; i<BUFFER_SIZE; ++i)
-            c = str_buffer(buffer, BUFFER_SIZE, getchar());
+        if (multi_line == INPAIR){
+          multi_line = (match(buffer, multi_comments[1]) == MATCHED? OUTPAIR:INPAIR);
+          if (multi_line == OUTPAIR)
+            for (i=0; multi_comments[1][i] != '\0'; ++i)
+              c = str_buffer(buffer, BUFFER_SIZE, getchar());
+        }else if (single_line == INPAIR){
+          single_line = (match(buffer, inline_comments[1]) == MATCHED? OUTPAIR:INPAIR);
+          if (single_line == OUTPAIR)
+            for (i=0; inline_comments[1][i] != '\0'; ++i)
+              c = str_buffer(buffer, BUFFER_SIZE, getchar());
+          /* By using this for part of the code, the program is expected to
+             omitting '\n', after the deletetion, the text could be different
+             but still correct in C Programming. */
+        }
       }
-      /*
-        if (match(buffer, inline_comments[0]) == 1)
-        comment = INPAIR;
-      */ /* there's no need to detect nested comments since
-            it is already INPAIR. */
       if (comment == OUTPAIR){
-        for (i=0; i < BUFFER_SIZE; ++i)
-          if (buffer[i] != multi_comments[0][i])
-            break;
-        comment = (i == BUFFER_SIZE? INPAIR:OUTPAIR);
+        if (multi_line == OUTPAIR)
+          multi_line = (match(buffer, multi_comments[0]) == MATCHED? INPAIR:OUTPAIR);
+        if (single_line == OUTPAIR)
+          single_line = (match(buffer, inline_comments[0]) == MATCHED? INPAIR:OUTPAIR);
       }
     }
+    comment = (single_line || multi_line); // Output the comment status.
     if (comment == OUTPAIR){
-        /* which means it is in quote that needs to be copied down directly, the
-           first expression needs to check the quote status because in its
-           quotation it must read and write the same char.
-        */
-        /* temp equals to NIL means there's no stored char that is undecided. */
-        putchar(c);
+      // which means it is OUT of PAIR that needs to be copied down directly.
+      putchar(c);
     }
   }
   if (comment == INPAIR)
@@ -84,3 +81,19 @@ int str_buffer(char s[], int len, int c)
   return s[0];
 }
 
+/* assume word array has len characters, the function check whether the first
+   len characters match the words. It is assumed that s has len or more
+   characters length. */
+int match(char s[], char w[]){
+  int i;
+
+  for (i=0; w[i] != '\0'; ++i)
+    if (s[i] != w[i])
+      break;
+  /* this uses a for loop to test whether it needs to jump out the comments. the
+     '\0' of string s is not used due to the initialization of the buffer to be
+     all '\0'. By using w[i], it is assumed that the match words are constructed
+     properly. ('\0' at the end)  */
+
+  return (w[i] == '\0'? MATCHED:UNMATCHED);
+}
